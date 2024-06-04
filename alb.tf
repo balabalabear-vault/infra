ec70ab10dd4b7f3d153ede582d02d91f4ca5e692
Kwok Yui Kuen <88313566+kuenyuikwok1106@users.noreply.github.com>
2024-06-04 16:23:04 -0400

    allow 80 and 443 port inbound

--- alb.tf
+++ alb.tf
@@ -27,8 +27,8 @@ resource "aws_alb_target_group" "app" {
     }
 }
 
-# Redirect all traffic from the ALB to the target group
-resource "aws_alb_listener" "front_end" {
+# Redirect port 3000 traffic from the ALB to the target group
+resource "aws_alb_listener" "app_port" {
   load_balancer_arn = aws_alb.main.id
   port              = var.app_port
   protocol          = "HTTP"
@@ -37,4 +37,37 @@ resource "aws_alb_listener" "front_end" {
     target_group_arn = aws_alb_target_group.app.id
     type             = "forward"
   }
+}
+
+# Redirect http traffic from the ALB to the target group
+resource "aws_alb_listener" "http" {
+  load_balancer_arn = aws_alb.main.id
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    target_group_arn = aws_alb_target_group.app.id
+    type             = "forward"
+  }
+}
+
+# Redirect https traffic from the ALB to the target group
+resource "aws_alb_listener" "https" {
+  load_balancer_arn = aws_alb.main.id
+  port              = 443
+  protocol          = "HTTPS"
+  default_action {
+    target_group_arn = aws_alb_target_group.app.id
+    type             = "forward"
+  }
+}
+
+resource "aws_lb_listener_certificate" "https" {
+  listener_arn    = aws_alb_listener.https.arn
+  certificate_arn = data.aws_acm_certificate.tossl.arn
+}
+
+resource "aws_lb_listener_certificate" "https-www" {
+  listener_arn    = aws_alb_listener.https.arn
+  certificate_arn = aws_acm_certificate.www.arn
 }
