9422f74d21f488c89a720d89939c8db2ef66edf7
Kwok Yui Kuen <88313566+kuenyuikwok1106@users.noreply.github.com>
2024-06-07 11:03:46 -0400

    cname for api. acm

--- route53.tf
+++ route53.tf
@@ -2,21 +2,6 @@ data "aws_route53_zone" "balabalabear" {
   name         = "balabalabear.com"
 }
 
-data "aws_acm_certificate" "tossl" {
-  domain   = "balabalabear.com"
-  types       = ["AMAZON_ISSUED"]
-  most_recent = true
-}
-
-resource "aws_acm_certificate" "www" {
-  domain_name       = "www.balabalabear.com"
-  validation_method = "DNS"
-
-  lifecycle {
-    create_before_destroy = true
-  }
-}
-
 resource "aws_route53_record" "com" {
   zone_id = data.aws_route53_zone.balabalabear.zone_id
   name    = "balabalabear.com"
@@ -38,5 +23,25 @@ resource "aws_route53_record" "www" {
     name                   = aws_alb.main.dns_name
     zone_id                = aws_alb.main.zone_id
     evaluate_target_health = true
+  }
+}
+
+resource "aws_route53_record" "api_acm" {
+  name    = tolist(aws_acm_certificate.api.domain_validation_options)[0].resource_record_name
+  type    = tolist(aws_acm_certificate.api.domain_validation_options)[0].resource_record_type
+  zone_id = data.aws_route53_zone.balabalabear.id # Replace with your Route 53 Hosted Zone ID
+  records = [tolist(aws_acm_certificate.api.domain_validation_options)[0].resource_record_value]
+  ttl     = 60
+}
+
+resource "aws_route53_record" "api" {
+  name    = aws_api_gateway_domain_name.api.domain_name
+  type    = "A"
+  zone_id = data.aws_route53_zone.balabalabear.id
+
+  alias {
+    evaluate_target_health = true
+    name                   = aws_api_gateway_domain_name.api.cloudfront_domain_name
+    zone_id                = aws_api_gateway_domain_name.api.cloudfront_zone_id
   }
 }
